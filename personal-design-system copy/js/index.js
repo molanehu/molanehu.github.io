ca00bed0476e52ce11087a90e3e022e15eb85b50
molanehu <molanehu@gmail.com>
2018-03-08 18:07:01 -0500

    copies

---
+++ personal-design-system copy/js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 1000, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+	
+});
+	
+$(window).scroll(function() {    
+	var scroll = $(window).scrollTop();
+
+	if (scroll >= 100) {
+		$(".fixed-header").slideDown();
+	} else {
+		$(".fixed-header").slideUp();
+	}
+});
